4df0065e04958475d197627884113586562d97dc
lubka1 <smolarova.lubka@gmail.com>
2025-12-04 07:31:00 +0100

    update config

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ activation = 'sigmoid'
 optim = keras.optimizers.Adam(LR)
 
 # the authors write "we found γ=2 to work best in our experiments."
-alpha = 0.7   #0.9   Class 1 (thaw slump) gets higher weight
+alpha = 0.9   #0.9   Class 1 (thaw slump) gets higher weight
 gamma = 2.0   # Focus more on hard-to-classify areas when higher, default 2, 
 focal_loss = sm.losses.BinaryFocalLoss(alpha=alpha, gamma=gamma)
 dice_loss = sm.losses.DiceLoss()
